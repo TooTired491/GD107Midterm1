8b37cb5e25c12554f0130d54e5524f4c9f3afd99
TooTired491 <Insomniaccat491@gmail.com>
2025-03-16 14:08:27 -0700

    Long time

---
+++ script.js
@@ -0,0 +1,16 @@
+//Beliefs
+let hoverElement1 = document.getElementById('button');
+hoverElement1.addEventListener('mouseenter', function() {
+	hoverElement1.style.opacity = '0.8';
+})
+hoverElement1.addEventListener('mouseleave', function() {
+	hoverElement1.style.opacity = '3';
+});
+//Quotes
+let hoverElement2 = document.getElementById('button2'); 
+hoverElement2.addEventListener('mouseenter', function() {
+	hoverElement2.style.opacity = '0.8';
+})
+hoverElement2.addEventListener('mouseleave', function() {
+	hoverElement2.style.opacity = '1';
+});
